b000095a0a2c1409e15bbfd34287f693c8ff1d02
woodley <gelinw3@gmail.com>
2023-06-16 15:44:42 -0400

    Part1 completed

--- assignment7/src/ZipComponent.jsx
+++ assignment7/src/ZipComponent.jsx
@@ -4,24 +4,6 @@ import axios from "axios";
 function ZipComponent(props) {
   const [zipCode, setZipCode] = useState("");
   const [zipInfo, setZipInfo] = useState(null);
-    // pokeList  === this.state.pokeList
-    // setPokeList === this.state.pokeList
-    //api url format https://zip-api.eu/api/v1/radius/[country_code]-[postal_code]/[distance]/[unit]
-    // useEffect(() => {
-    //     async function fetchZip() {
-    //         try {
-    //             //link should be in the form of https://zip-api.eu/api/v1/radius/US-11373/5/km
-    //             //try 5 km first
-    //             const list = await axios.get(`https://zip-api.eu/api/v1/radius/US-${zipCode}/5/km`);
-    //             setZipCode(list.data.results);
-    //             // this.setState(list.data.results)
-    //         } catch (error) {
-    //             console.error(error);
-    //         }
-    //     }
-    //     fetchZip();
-    // }, []);
-    // console.log(props);
 
     async function handleSubmit(event){
       event.preventDefault();
@@ -30,14 +12,13 @@ function ZipComponent(props) {
                 //link should be in the form of https://zip-api.eu/api/v1/radius/US-11373/5/km
                 //try 5 km first
                 const list = await axios.get(`https://zip-api.eu/api/v1/radius/US-${zipCode}/5/km`);
-                setZipInfo(list.data.results);
+                setZipInfo(list.data);
                 console.log("testing", list);
-
+                console.log("testing data", list.data);
                 // this.setState(list.data.results)
             } catch (error) {
                 console.error(error);
             }
-      //call fetchzip here
     }
 
     function handleChange(event){
@@ -53,6 +34,16 @@ function ZipComponent(props) {
         </input>
         <button type="submit" onClick={handleSubmit}>submit</button>
       </form>
+      <h1>Cities</h1>
+      {zipInfo?.map((zipData, ind)=>{
+        return(
+        <div  key={zipData.postal_code}>
+          <p>Postal Code:{zipData.postal_code}</p>
+          <p>State:{zipData.state}</p>  
+          <p>city:{zipData.place_name}</p>
+          <br></br>
+        </div>)
+      })}
     </div>
   )
 }
