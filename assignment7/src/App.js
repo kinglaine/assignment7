0f5c271640a89c18b766f17cddab639cc8191e59
woodley <gelinw3@gmail.com>
2023-06-16 14:00:45 -0400

    setup

--- assignment7/src/App.js
+++ assignment7/src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import ZipComponent from './ZipComponent';
 
 function App() {
   return (
-    <p>hello</p>
+    <ZipComponent></ZipComponent>
   );
 }
 
