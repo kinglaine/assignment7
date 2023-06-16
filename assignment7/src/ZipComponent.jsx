76e931517b4567d3841047a0cd60c326d6224ad9
sabinaismailova <sabina.ismailova88@bcmail.cuny.edu>
2023-06-16 14:24:04 -0400

    changed zip-component contents

--- assignment7/src/ZipComponent.jsx
+++ assignment7/src/ZipComponent.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
-function ZipComponent() {
+function ZipComponent(props) {
+  const [zipCode, setZipCode] = useState([]);
+    // pokeList  === this.state.pokeList
+    // setPokeList === this.state.pokeList
+
+    useEffect(() => {
+        async function fetchZip() {
+            try {
+                const list = await axios.get("");
+                setZipCode(list.data.results);
+                // this.setState(list.data.results)
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchZip();
+    }, []);
+    console.log(props);
+
+    function handleSubmit(){
+      console.log("submitted");
+    }
+
+    function handleChange(event){
+      console.log(event.target.value);
+      setZipCode(event.target.value);
+    }
+
   return (
-    <div>zipComponent</div>
+    <div>
+      <form >
+        <input onChange={handleChange}>
+
+        </input>
+        <button type="submit" onClick={handleSubmit}>submit</button>
+      </form>
+    </div>
   )
 }
 
